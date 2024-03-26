ec35fa3c21b2254bbe394d727048069ad5940f5d
Melissa Hale <melissakayh@gmail.com>
2024-03-26 13:40:12 -0500

    Update index.js

--- fe/index.js
+++ fe/index.js
@@ -27,7 +27,7 @@ app.get('/fetch-secret', async (req, res) => {
 
 app.get('/', async (req, res) => {
     res.send(`
-        <h1>Secret Rotation Demo</h1>
+        <h1>Secret Rotation Demo 2</h1>
         <button onclick="fetchSecret()">Get Secret</button>
         <div id="secretDisplay"></div>
         <script>
